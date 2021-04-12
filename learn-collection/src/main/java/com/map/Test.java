ac47649ba04a9cdc741d2b0dcab41ed369f42218
gaoyongsheng <2409546279@qq.com>
2021-04-12 23:45:43 +0800

    基础模块构建

---
+++ learn-collection/src/main/java/com/map/Test.java
@@ -0,0 +1,13 @@
+package com.map;
+
+/**
+ * @author gys
+ * @version 1.0
+ * @date 2021/4/12 下午11:44
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("11");
+    }
+}
